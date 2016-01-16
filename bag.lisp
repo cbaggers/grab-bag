c33b8b91207fa78f7b6491a482262ce7d2690193
Baggers <baggers@sett.lan>
2016-01-16 20:26:33 +0100

    optimize remove-item

--- bag.lisp
+++ bag.lisp
@@ -1,5 +1,7 @@
 (in-package #:grab-bag)
 
+(defconstant +release-mode+ nil)
+
 ;; # NOTES FOR TYPED VERSION
 ;;
 ;; - take a function for comparison so #'add-item doesnt need arg
@@ -10,14 +12,19 @@
   ;; the master array of items in the bag
   (items (%make-items-array) :type (array t (*)))
 
-  ;; a list of empty slots in the above array
-  (holes-in-items nil :type list)
-
   ;; a holeless array with the same elements as items
   (item-cache (%make-item-cache) :type (array t (*)))
 
-  ;; quick lookup for cache state
-  (cache-invalid-p nil :type boolean)
+  ;; an array where the nth element hold the index into the
+  ;; item-cache where you can find the nth element of 'items
+  ;; this is used to optimize removing from the cache without
+  ;; having to mark it dirty and regenerate.
+  (items-to-cache-indices (%make-item-to-cache-array) :type (array fixnum (*)))
+
+  ;; a list of empty slots in the items array
+  ;; this is used in add-item to avoid extending the array if
+  ;; elements are avaiable
+  (holes-in-items nil :type list)
 
   ;; called when item added
   (added-callbacks (%make-callback-array)
@@ -51,6 +58,9 @@
 		    :initial-contents items))
       (make-array 0 :adjustable t :fill-pointer 0)))
 
+(defun %make-item-to-cache-array ()
+  (make-array 0 :element-type 'fixnum :adjustable t :fill-pointer 0))
+
 (defun %make-callback-array ()
   (make-array 0 :element-type '(function (%bag t) t)
 	      :adjustable t :fill-pointer 0))
@@ -66,21 +76,10 @@
 	(%bag-removed-callbacks bag) (%make-callback-array))
   nil)
 
-;;----------------------------------------------------------------------
-
-(defun %fix-up-cache (bag)
-  (setf (%bag-item-cache bag)
-	(%make-item-cache
-	 (loop :for item :across (%bag-items bag)
-	    :when (not (null item)) :collect item)))
-  (setf (%bag-cache-invalid-p bag) nil)
-  bag)
 
 ;;----------------------------------------------------------------------
 
 (defun get-items (bag)
-  (when (%bag-cache-invalid-p bag)
-    (%fix-up-cache bag))
   (%bag-item-cache bag))
 
 ;;----------------------------------------------------------------------
@@ -90,11 +89,13 @@
     (let ((extension (%bag-extension bag)))
       (if (%bag-holes-in-items bag)
 	  (let ((i (pop (%bag-holes-in-items bag))))
-	    (setf (aref (%bag-items bag) i) item))
-	  (vector-push-extend item (%bag-items bag) extension))
-      (if (%bag-cache-invalid-p bag)
-	  (%fix-up-cache bag)
-	  (vector-push-extend item (%bag-item-cache bag) extension))))
+	    (setf (aref (%bag-items bag) i) item
+		  (aref (%bag-items-to-cache-indices bag) i) item))
+	  (progn
+	    (vector-push-extend (length (bag-item-cache bag))
+				(bag-items-to-cache-indices bag))
+	    (vector-push-extend item (%bag-items bag) extension)))
+      (vector-push-extend item (%bag-item-cache bag) extension)))
   (loop :for rummager :across (bag-rummagers bag) :do
      (%parent-on-added bag item))
   (loop :for callback :across (%bag-added-callbacks bag) :do
@@ -107,9 +108,8 @@
   (let ((pos (position item (%bag-items bag) :test test)))
     (if pos
 	(progn
-	  (setf (aref (%bag-items bag) pos) nil
-		(%bag-item-cache bag) (%make-item-cache)
-		(%bag-cache-invalid-p bag) t)
+	  (setf (aref (%bag-items bag) pos) nil)
+	  (%remove-item-from-cache bag pos)
 	  (push pos (%bag-holes-in-items bag))
 	  (loop :for rummager :across (bag-rummagers bag) :do
 	     (%parent-on-removed bag item))
@@ -118,16 +118,36 @@
 	  bag)
 	(error "No item ~s in bag ~s" item bag))))
 
-;; (defun %remove-item-from-cache (bag item)
-;;   ;; swap with last element and (1- fill-pointer)
-;;   )
+(defun %remove-item-from-cache (bag item-pos)
+  ;; swap with last element and (1- fill-pointer)
+  (let* ((item-cache (%bag-item-cache bag))
+	 (last-pos (1- (length item-cache)))
+	 (last-cache-item (aref item-cache last-pos))
+
+	 (index-cache (%bag-items-to-cache-indices bag))
+	 (cache-remove-pos (aref index-cache item-pos))
+
+	 ;; find the position in the item list that the last-cache-item
+	 (item-moved-pos (position last-cache-item (%bag-items bag))))
+    ;; take the last item from the cache and place it in the slot
+    ;; where the item being removed was
+    (setf (aref item-cache cache-remove-pos) last-cache-item)
+
+    ;; update the item->cache index for the moved item
+    (setf (aref index-cache last-pos) (aref index-cache item-moved-pos))
+    (unless +release-mode+
+      (setf (aref index-cache item-pos) -1))
+
+    ;; the item cache and item->cache map now need shortening
+    (decf (fill-pointer item-cache)))
+  bag)
+
 
 (defun remove-all (bag)
   (let ((items (%bag-items bag)))
     (setf (%bag-items bag) (%make-items-array)
 	  (%bag-item-cache bag) (%make-item-cache)
 	  (%bag-holes-in-items bag) nil
-	  (%bag-cache-invalid-p bag) nil
 	  (%bag-added-callbacks bag) (%make-callback-array)
 	  (%bag-removed-callbacks bag) (%make-callback-array))
     (loop :for rummager :across (bag-rummagers bag) :do
@@ -155,3 +175,19 @@
 (defun %remove-rummager (bag rummager)
   (vector-push-extend rummager (bag-rummagers bag))
   bag)
+
+;;----------------------------------------------------------------------
+
+(defun rummage (bag predicate)
+  (let ((r (make-rummage :predicate predicate)))
+    (%add-rummager bag r)
+    r))
+
+(defun stop-rummaging (rummage)
+  )
+
+(defun %parent-on-added (parent item)
+  )
+
+(defun %parent-on-removed (parent item)
+  )
